300d5dc344d752adde9cc5d25ecbd164852cbea2
EmilianoZ96 <129908439+EmilianoZ96@users.noreply.github.com>
2023-06-21 13:27:15 -0300

    Add files via upload

---
+++ Ejercicio3Guia11/src/Enridad/Palo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Enridad;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Palo {
+    
+    private String palo;
+
+    public Palo() {
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+
+    @Override
+    public String toString() {
+        return "Palo{" + "palo=" + palo + '}';
+    }
+
+ 
+    
+    
+    
+}
